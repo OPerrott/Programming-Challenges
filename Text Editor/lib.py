3c1e5df0bc5b2c22ae13c7fcd39b3cc1190e794f
OPerrott <135993193+OPerrott@users.noreply.github.com>
2025-05-14 10:58:51 +0100

    Add files via upload

---
+++ Text Editor/lib.py
@@ -0,0 +1,9 @@
+from tkinter import *   # For the application window
+from tkinter import ttk # For the application window
+
+from tqdm import tqdm   # For download progress bar
+
+import requests     # For web scraping
+import math         # For advanced maths calculations
+import time         # For recording time-complexity / performance 
+import os           # For terminal manipulation
